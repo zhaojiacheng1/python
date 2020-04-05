f3c458c9e9bc865b76d58df4e5abf61ca32b4f8e
zhaojiacheng1 <zhaojiacheng1@outlook.com>
2020-04-05 23:24:47 +0800

    PyQt5综合案例3

---
+++ PyQt_Demo/Caculator_Btn.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from PyQt5.Qt import *
+import sys
+
+
+class CaculatorBtn(QPushButton):
+	key_pressed = pyqtSignal(str, str)
+
+	def __init__(self, parent=None, *args, **kwargs):
+		super().__init__(parent, *args, **kwargs)
+		# self.setAutoExclusive(True)  # 设置排他
+		pass
+
+	def resizeEvent(self, *args, **kwargs):
+		super().resizeEvent(*args, **kwargs)
+		self.setStyleSheet("""
+			QPushButton[bg='gray'] {
+				color: white;
+				background-color: rgb(88, 88, 88);
+			}
+			QPushButton[bg='gray']:hover {
+				background-color: rgb(150, 150, 150);
+			}
+			QPushButton[bg='orange'], QPushButton[bg='equal'] {
+				color: white;
+				background-color: rgb(207, 138, 0);
+			}
+			QPushButton[bg='orange']:hover, QPushButton[bg='equal']:hover {
+				background-color: rgb(238, 159, 0);
+			}
+			QPushButton[bg='orange']:checked {
+				color: rgb(207, 138, 0);
+				background-color: white;
+			}
+			QPushButton[bg='lightgray'] {
+				color: black;
+				background-color: rgb(200, 200, 200);
+			}
+			QPushButton[bg='lightgray']:hover {
+				background-color: rgb(230, 230, 230);
+			}
+			QPushButton[bg] {
+				font-size: 20px;
+				border-radius: %dpx;
+			}
+		""" % (min(self.width(), self.height()) / 2))
+		pass
+
+	def mousePressEvent(self, *args, **kwargs):
+		super().mousePressEvent(*args, **kwargs)
+		self.key_pressed.emit(self.text(), self.property('role'))
+		pass
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	window = QWidget()
+	window.show()
+	sys.exit(app.exec_())
--- PyQt_Demo/Caculator_pane.py
+++ PyQt_Demo/Caculator_pane.py
@@ -5,11 +5,114 @@ from PyQt5.Qt import *
 from resource.caculator import Ui_Form
 
 
+class Caculator(QObject):
+	show_content = pyqtSignal(str)
+
+	def __init__(self, parent):
+		super().__init__(parent)
+		self.key_models = [ ]
+		pass
+
+	def key_model_to_str(self):
+		expression = ''
+		if len(self.key_models) == 0:
+			expression = '0'
+			print(expression)
+			return expression
+		for model in self.key_models:
+			if model[ 'role' ] == 'operator':
+				expression += ' '
+				expression += model[ 'title' ]
+				expression += ' '
+			else:
+				expression += model[ 'title' ]
+		# print(expression)
+		return expression
+
+	def caculate(self):
+		if len(self.key_models) > 0 and self.key_models[ -1 ][ 'role' ] == 'operator':
+			self.key_models.pop(-1)  # 删除最后一个
+		expression = ''
+		for model in self.key_models:
+			expression += model[ 'title' ]
+		result = eval(expression)  # 计算字符串算式，返回值为计算结果
+		self.key_models.clear()
+		self.key_models.append({ 'title': '{}'.format(result), 'role': 'num' })
+		# print(result)
+		return result
+		pass
+
+	def parse_key_model(self, key_model):
+		if key_model[ 'role' ] == 'clear':
+			self.key_models.clear()
+			# print(self.key_models)
+			self.show_content.emit(self.key_model_to_str())
+			return None
+		if key_model[ 'role' ] == 'caculate':
+			result = self.caculate()
+			# print(self.key_models)
+			self.show_content.emit(str(result))
+			return None
+		if len(self.key_models) == 0:
+			if key_model[ 'role' ] == 'num':
+				if key_model[ 'title' ] == '.':
+					key_model[ 'title' ] = '0.'
+				self.key_models.append(key_model)
+			# print(self.key_models)
+			self.show_content.emit(self.key_model_to_str())
+			return None
+		if key_model[ 'title' ] in ('%', '+/-'):
+			if self.key_models[ -1 ][ 'role' ] != 'num':
+				# print(self.key_models)
+				self.show_content.emit(self.key_model_to_str())
+				return None
+			else:
+				if key_model[ 'title' ] == '%':
+					self.key_models[ -1 ][ 'title' ] = str(float(self.key_models[ -1 ][ 'title' ]) / 100)
+				else:
+					self.key_models[ -1 ][ 'title' ] = str(float(self.key_models[ -1 ][ 'title' ]) * -1)
+				# print(self.key_models)
+				self.show_content.emit(self.key_model_to_str())
+			return None
+		if key_model[ 'role' ] == self.key_models[ -1 ][ 'role' ]:
+			if key_model[ 'role' ] == 'num':
+				if key_model[ 'title' ] == '.' and self.key_models[ -1 ][ 'title' ].__contains__('.'):
+					# print(self.key_models)
+					self.show_content.emit(self.key_model_to_str())
+					return None
+				if self.key_models[ -1 ][ 'title' ] != '0':
+					self.key_models[ -1 ][ 'title' ] += key_model[ 'title' ]
+				else:
+					if key_model[ 'title' ] == '.':
+						self.key_models[ -1 ][ 'title' ] += key_model[ 'title' ]
+					else:
+						self.key_models[ -1 ][ 'title' ] = key_model[ 'title' ]
+			if key_model[ 'role' ] == 'operator':
+				self.key_models[ -1 ][ 'title' ] = key_model[ 'title' ]
+		else:
+			self.key_models.append(key_model)
+		self.show_content.emit(self.key_model_to_str())
+		# print(self.key_models)
+		pass
+
+
 class CaculatorPane(QWidget, Ui_Form):
 	def __init__(self, parent=None, *args, **kwargs):
 		super().__init__(parent, *args, **kwargs)
 		self.setAttribute(Qt.WA_StyledBackground, True)  # 设置属性，使资源文件可以加载
 		self.setupUi(self)
+		self.caculator = Caculator(self)
+		self.lineEdit.setText('0')
+		self.caculator.show_content.connect(self.show_content)
+		pass
+
+	def show_content(self, content):
+		self.lineEdit.setText(content)
+		pass
+
+	def get_key(self, title, role):
+		# print(title, role)
+		self.caculator.parse_key_model({ 'title': title, 'role': role })
 		pass
 
 
